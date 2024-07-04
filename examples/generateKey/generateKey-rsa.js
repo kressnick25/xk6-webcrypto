7283d52f4e58ed6ea498f06dafde6b42f1c7f605
nkress <kressnick25@gmail.com>
2024-07-04 20:52:22 +1000

    Add more RSA generation tests

--- examples/generateKey/generateKey-rsa.js
+++ examples/generateKey/generateKey-rsa.js
@@ -1,16 +1,39 @@
 import { crypto } from "k6/x/webcrypto";
 
 export default async function () {
-  const key = await crypto.subtle.generateKey(
+  const keyPkcs = await crypto.subtle.generateKey(
     {
-      name: "RSA-OAEP",
-      modulusLength: 4096,
+      name: "RSASSA-PKCS1-v1_5",
+      modulusLength: 1024,
       publicExponent: new Uint8Array([1, 0, 1]),
       hash: "SHA-256",
     },
     true,
+    ["sign", "verify"]
+  );
+  console.log(JSON.stringify(keyPkcs));
+
+  const keyPss = await crypto.subtle.generateKey(
+    {
+      name: "RSA-PSS",
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: "SHA-384",
+    },
+    true,
+    ["sign", "verify"]
+  );
+  console.log(JSON.stringify(keyPss));
+
+  const keyOaep = await crypto.subtle.generateKey(
+    {
+      name: "RSA-OAEP",
+      modulusLength: 4096,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: "SHA-512",
+    },
+    true,
     ["encrypt", "decrypt"]
   );
-
-  console.log(JSON.stringify(key));
+  console.log(JSON.stringify(keyOaep));
 }
